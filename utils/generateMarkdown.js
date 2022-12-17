0fdef57b2ab809df9e0009a18c623e1810c47462
Liquid Monks <info@liquidmonks.com>
2022-12-17 09:03:58 -0600

    generateMarkdown.js includes a conditional statement that returns an a license section if the user chooses a section or returns an empty string if the user does not choose a license.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "none") {
+    return "";
+  } else {
+    return `## [License](#license)     
+    This project is licensed under the ${license} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
