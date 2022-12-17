fa46ab444d7690e1e1023bb0d8e0ff3947be4140
Liquid Monks <info@liquidmonks.com>
2022-12-17 08:23:05 -0600

    Project folder includes inquirer node package, an index.js file and generateMarkdown.js file that builds the general framework of the readme generator.

---
+++ index.js
@@ -0,0 +1,78 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide installation instructions.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide usage information.",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please provide contribution guidelines.",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide test instructions.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select a license.",
+    choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please provide your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address.",
+  },
+];
+const inquirer = require("inquirer");
+
+inquirer.prompt([,]).then((answers) => {
+  console.log(answers);
+  writeToFile("README.md", answers);
+});
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  const markdown = generateMarkdown(data);
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    writeToFile("README.md", answers);
+  });
+}
+
+// Function call to initialize app
+init();
