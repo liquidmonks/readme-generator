1dabcd862a6e8d0bfd6fa086f2459615e7bbf8be
Liquid Monks <info@liquidmonks.com>
2022-12-17 08:53:36 -0600

    generateMarkdown.js includes a conditional statement that returns an a license badge if a license is chose and an empty string if the user does not choose a license.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  if (license === "none") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
