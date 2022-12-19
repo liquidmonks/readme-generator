e3162a28dead9cf390469343a954d385b0f4301d
Liquid Monks <info@liquidmonks.com>
2022-12-19 17:42:13 -0600

    generateMarkdown.js includes additional dev comments that clarifies the markdown template of the script.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,9 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+
 function renderLicenseLink(license) {
+  // Function to render the license link
   if (license === "none") {
     return "";
   } else {
@@ -21,6 +23,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  // Function to render the license section
   if (license === "none") {
     return "";
   } else {
@@ -30,12 +33,15 @@ function renderLicenseSection(license) {
 }
 
 // TODO: Create a function to generate markdown for README
+
+/*START: **********************MARKDOWN GENERATOR TEMPLATE**********************/
 function generateMarkdown(data) {
+  // Function to generate the markdown
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
 
 
-## Table-of-Contents
+## Table-of-Contents  
 * [Description](#description)
 * [Installation](#installation)
 * [Usage](#usage)
@@ -44,28 +50,38 @@ ${renderLicenseBadge(data.license)}
 ${renderLicenseLink(data.license)}
 * [Questions](#questions)
 
+
 ## [Description](#table-of-contents)
 ${data.description}
+### Screenshot
+![App Screenshot](./assets/images/screenshot.png)
+
 
 
 ## [Installation](#table-of-contents)
 To install necessary dependencies, run the following command:
 ${data.installation}
 
+
 ## [Usage](#table-of-contents)
 ${data.usage}
 ${renderLicenseSection(data.license)}
 
+
 ## [Contribution](#table-of-contents)
 ${data.contribution}
+
 
 ## [Test](#table-of-contents)
 To run tests, run the following command:
 ${data.test}
 
+
 ## [Questions](#table-of-contents)
 If you have any questions, please contact me at ${data.email} or visit my GitHub page at [${data.github}]       
 `;
 }
+/***********************MARKDOWN GENERATOR TEMPLATE*********************: END*/
 
+// Exporting the generateMarkdown function
 module.exports = generateMarkdown;
